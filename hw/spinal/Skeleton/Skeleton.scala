11af070dad20ecff7f5b10fc0146d75d74283d57
Yan Pan <ypan2003@yeah.net>
2023-12-13 04:29:26 +0800

    [MISC]: Project Topmodule

---
+++ hw/spinal/Skeleton/Skeleton.scala
@@ -0,0 +1,28 @@
+package Skeleton
+
+import spinal.core._
+
+import config._
+
+// Hardware definition
+case class Skeleton() extends Component {
+  val io = new Bundle {
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
+    val state = out UInt(8 bits)
+  }
+
+  val counter = Reg(UInt(8 bits)) init 0
+
+  when(io.cond0) {
+    counter := counter + 1
+  }
+
+  io.state := counter
+  io.flag := (counter === 0) | io.cond1
+}
+
+object SkeletonVerilog extends App {
+  Config.spinal.generateVerilog(Skeleton())
+}
