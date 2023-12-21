7fdcdd54f4d21ef8772ffb2c1a7bb9de31cdf7e1
Yan Pan <ypan2003@yeah.net>
2023-12-21 18:51:20 +0800

    [Commit]: Basic commit logic

---
+++ hw/spinal/Skeleton/backend/Commit.scala
@@ -0,0 +1,25 @@
+package Skeleton.backend
+
+import spinal.core._
+import spinal.lib._
+
+import Skeleton.bundle._
+import Skeleton.config._
+
+case class CommitLogic(fuType: SpinalEnumElement[FUType.type], config: CPUConfig) extends Component {
+    val io = new Bundle {
+        val input = slave Stream(FUWBBundle(config))
+        val srat = master(RATIOBundle(false, true, config))
+        val rob = master(ROBCommitIOBundle(config))
+        val prf = master(PRFIOBundle(true, config))
+    }
+    io.input.ready := True
+    io.srat.prd := io.input.payload.prd
+    io.srat.valid := io.input.valid
+    io.rob.robIdx := io.input.payload.robIdx
+    io.rob.branchResult := io.input.payload.branchResult
+    io.rob.exceptionInfo := io.input.payload.exceptionInfo
+    io.rob.valid := io.input.valid
+    io.prf.idx := Mux(io.input.valid, io.input.payload.prd, B(0).resized)
+    io.prf.data := io.input.payload.data
+}
