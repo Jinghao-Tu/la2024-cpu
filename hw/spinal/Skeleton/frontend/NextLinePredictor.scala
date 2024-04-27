05cbecd60d843d1735ecfc97b742734a8166bd83
Yan Pan <ypan2003@yeah.net>
2024-04-27 22:11:37 +0800

    [BPU]: Predictor port tune

--- hw/spinal/Skeleton/frontend/NextLinePredictor.scala
+++ hw/spinal/Skeleton/frontend/NextLinePredictor.scala
@@ -9,10 +9,10 @@ import Skeleton.config._
 case class NextLinePredictor(config: CPUConfig) extends Component {
     val io = new Bundle {
         val pc = Vec.fill(config.fetchWidth)(slave Flow(UInt(config.valen bits))) // 0-latency!
-        val branchInfo = Vec.fill(config.fetchWidth)(master Flow(BranchInfo(config))) // 0-latency!
+        val npc = Vec.fill(config.fetchWidth)(master Flow(UInt(config.valen bits))) // 0-latency!
+        val branchInfo = out(Vec.fill(config.fetchWidth)(BranchInfo(config)))
         val updateInfo = Vec.fill(config.retireWidth)(slave Flow(BPUUpdateBundle(config))) // 0-latency!
     }
-
     val fetchMask = Bits(config.fetchWidth bits)
     val nextBase = UInt(config.valen bits)
     val lastPCIdx = UInt(log2Up(config.fetchWidth) bits)
@@ -21,8 +21,9 @@ case class NextLinePredictor(config: CPUConfig) extends Component {
 
     (0 until config.fetchWidth).map(i => {
         fetchMask(i) := io.pc(i).valid
-        io.branchInfo(i).valid := True
-        io.branchInfo(i).payload.predictPC := nextBase + i|<<log2Up(config.instLength/8)
-        io.branchInfo(i).payload.predictResult := False
+        io.npc(i).valid := True
+        io.npc(i).payload := nextBase + i|<<log2Up(config.instLength/8)
+        io.branchInfo(i).predictPC := U(0).resized // Don't care on not taken
+        io.branchInfo(i).predictResult := False
     })
 }
