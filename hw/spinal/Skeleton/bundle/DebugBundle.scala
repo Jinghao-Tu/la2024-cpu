4e9509b5a5833874f7b5cb48b971d79b37453419
Yan Pan <ypan2003@yeah.net>
2024-04-28 22:58:19 +0800

    [MISC]: Fix spell change

--- hw/spinal/Skeleton/bundle/DebugBundle.scala
+++ hw/spinal/Skeleton/bundle/DebugBundle.scala
@@ -9,14 +9,14 @@ case class LSDebugBundle(config: CPUConfig) extends Bundle {
     val wb_pc = Bits(config.valen bits)
     val wb_rf_wen = Bits(4 bits)
     val wb_rf_wnum = Bits(5 bits)
-    val wb_rd_wdata = Bits(config.wordLength bits)
+    val wb_rf_wdata = Bits(config.wordLength bits)
 
     def defaultVal: LSDebugBundle = {
         val value = LSDebugBundle(config)
         value.wb_pc := B(0).resized
         value.wb_rf_wen := B(0).resized
         value.wb_rf_wnum := B(0).resized
-        value.wb_rd_wdata := B(0).resized
+        value.wb_rf_wdata := B(0).resized
         return value
     }
 }
