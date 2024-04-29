a18bb0c9b02fc772f797aa4fe5bbcc9a4f5ca4db
Yan Pan <ypan2003@yeah.net>
2024-04-29 20:05:09 +0800

    [RO]: Now bahaves correctly on BL inst

--- hw/spinal/Skeleton/backend/ReadOperand.scala
+++ hw/spinal/Skeleton/backend/ReadOperand.scala
@@ -117,7 +117,7 @@ case class ReadOperandLogic(iqType: SpinalEnumElement[FUType.type], config: CPUC
                 io.toFU.src1 := pc
                 io.toFU.src2 := U(4).resized
                 io.toFU.src3 := imm |<< 2
-                io.toFU.src4 := reg1
+                io.toFU.src4 := pc
             }
         }
     } else if (iqType == FUType.mulu || iqType == FUType.divu) {
