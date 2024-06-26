1aeaac550f0441388e6207b8392dfbc2c80f126a
Jinghao Tu <jinghaotu213@jht213.com>
2024-06-26 11:27:32 +0800

    add Ras Predictor

---
+++ hw/spinal/Skeleton/frontend/RasPredictor.scala
@@ -0,0 +1,102 @@
+package Skeleton.frontend
+
+import spinal.core._
+import spinal.lib._
+
+import Skeleton.bundle._
+import Skeleton.config._
+
+// * 用于函数调用和返回地址的栈结构预测器
+// RAS 是一个寄存器堆实现的栈存储结构，它对 call 指令的下一条指令的地址进行记录，在 FTB 认为预测块会在 call 指令跳转时压栈，并在 FTB 认为预测块在 ret 指令跳转时弹出。每一项包含一个地址和一个计数器，当重复压栈同一个地址时，栈指针不变，计数器加一，用于处理程序中递归调用的情况。每次预测后，栈顶项和栈指针都会存入 FTQ 的存储结构，用于误预测时恢复
+case class RasPredictor(config: CPUConfig) extends Component {
+    val io = new Bundle {
+        val pc = Vec.fill(config.fetchWidth)(slave Flow(UInt(config.valen bits))) // 0-latency!
+        val npc = Vec.fill(config.fetchWidth)(master Flow(UInt(config.valen bits))) // 0-latency!
+        val branchInfo = out(Vec.fill(config.fetchWidth)(BranchInfo(config)))
+        val updateInfo = Vec.fill(config.retireWidth)(slave Flow(BPUUpdateBundle(config))) // 0-latency!
+    }
+    val fetchMask = Bits(config.fetchWidth bits)
+    val nextBase = UInt(config.valen bits)
+    val lastPCIdx = UInt(log2Up(config.fetchWidth) bits)
+    lastPCIdx := OHToUInt(OHMasking.last(fetchMask))
+    nextBase := io.pc(lastPCIdx).payload + 4
+
+    (0 until config.fetchWidth).map(i => {
+        fetchMask(i) := io.pc(i).valid
+    })
+    
+    val callTable = Mem(UInt(config.rasTableWidth bits), wordCount = config.rasTableSize)
+    val retTable = Mem(UInt(config.rasTableWidth bits), wordCount = config.rasTableSize)
+    val retStack = Mem(UInt(config.rasStackWidth bits), wordCount = config.rasStackDepth)
+    val retStackPtr = Reg(UInt(log2Up(config.rasStackDepth) bits)) // Ptr -1 is the top of the stack, Ptr is the next push position
+    
+    def hash_tag(pc: UInt): UInt = {
+        var hash = U(0, 8 bits)
+        for (i <- 0 until 4) {
+            hash = hash ^ pc(i * 8 + 7 downto i * 8)
+        }
+        hash
+    }
+    
+// ------------------------------- predict -------------------------------
+    val callHit = Vec.fill(config.fetchWidth)(Bool)
+    val retHit = Vec.fill(config.fetchWidth)(Bool)
+    val pushData = U(0, config.rasStackWidth bits)
+    val pushFlag = False
+    val popFlag = False
+    
+    (0 until config.fetchWidth).map(i => {
+        val index = io.pc(i).payload(11 downto 2)
+        val tag = hash_tag(io.pc(i).payload)
+        callHit(i) := callTable.readAsync(index)(7 downto 0) === tag && callTable.readAsync(index)(8)
+        retHit(i) := retTable.readAsync(index)(7 downto 0) === tag && retTable.readAsync(index)(8)
+    })
+    
+    (config.fetchWidth - 1 until -1 by -1).map(i => {
+        when(fetchMask(i) && callHit(i)) {
+            pushData := io.pc(i).payload + config.valen/8
+            pushFlag := True
+        }
+        when(fetchMask(i) && retHit(i)) {
+            popFlag := True
+        }
+    })
+    
+    when(pushFlag) {
+        retStack.write(retStackPtr, pushData)
+        retStackPtr := retStackPtr + 1
+    }
+    when(popFlag) {
+        nextBase := retStack.readSync(retStackPtr - 1)
+        retStackPtr := retStackPtr - 1
+    }
+    
+    (0 until config.fetchWidth).map(i => {
+        io.npc(i).valid := fetchMask(i)
+        io.npc(i).payload := nextBase
+        io.branchInfo(i).predictPC := nextBase + i |<< log2Up(config.valen / 8)
+        io.branchInfo(i).predictResult := callHit(i) || retHit(i)
+        io.branchInfo(i).GHR := 0
+    })
+
+// ------------------------------- update -------------------------------
+    val updateCallFetchMask = Bits(config.fetchWidth bits)
+    val updateRetFetchMask = Bits(config.fetchWidth bits)
+
+    (0 until config.fetchWidth).map(i => {
+        updateCallFetchMask(i) := io.updateInfo(i).valid && io.updateInfo(i).isCallInst
+        updateRetFetchMask(i) := io.updateInfo(i).valid && io.updateInfo(i).isRetInst
+    })
+    
+    (0 until config.fetchWidth).map(i => {
+        val index = io.pc(i).payload(11 downto 2)
+        val tag = hash_tag(io.pc(i).payload)
+        when(updateCallFetchMask(i)) {
+            callTable.write(index, True.asUInt @@ tag)
+        }
+        when(updateRetFetchMask(i)) {
+            retTable.write(index, True.asUInt @@ tag)
+        }
+    })
+
+}
